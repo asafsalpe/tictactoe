117c422d70089dac7c9a874262e693c7d87b4d2d
nubs29 <edzusvitols@gmail.com>
2023-11-23 14:36:21 +0200

    Update .py

--- .py
+++ .py
@@ -1,11 +1,50 @@
 from tkinter import*
 from tkinter import messagebox
+
 myWindow=Tk()
 myWindow.title("TicTacToe")
 myWindow.config()
 
 playerX = True
 count = 0
+
+def disableButtons():
+    ul.config(state=DISABLED)
+    um.config(state=DISABLED)
+    ur.config(state=DISABLED)
+    ml.config(state=DISABLED)
+    mm.config(state=DISABLED)
+    mr.config(state=DISABLED)
+    bl.config(state=DISABLED)
+    bm.config(state=DISABLED)
+    br.config(state=DISABLED)
+    return 0
+
+def reset():
+    ul.config(state=NORMAL)
+    um.config(state=NORMAL)
+    ur.config(state=NORMAL)
+    ml.config(state=NORMAL)
+    mm.config(state=NORMAL)
+    mr.config(state=NORMAL)
+    bl.config(state=NORMAL)
+    bm.config(state=NORMAL)
+    br.config(state=NORMAL)
+    ul["text"]=''
+    um["text"]=''
+    ur["text"]=''
+    bl["text"]=''
+    bm["text"]=''
+    br["text"]=''
+    ml["text"]=''
+    mm["text"]=''
+    mr["text"]=''
+    
+    return 0 
+
+def infoWindow():
+    newWindow=Toplevel()
+    newWindow
 
 def btnClick(button):
     global playerX, count
@@ -36,6 +75,8 @@ def checkWinner():
         ur["text"]=='X' and mr["text"]=='X'and br["text"]=='X'):
         winnner=True
         messagebox.showinfo("TicTacToe", "Player X wins!")
+        disableButtons()
+        
     elif (ul["text"]=='O' and um["text"]=='O'and ur["text"]=='O'or
         ml["text"]=='O' and mm["text"]=='O'and mr["text"]=='O'or
         bl["text"]=='O' and bm["text"]=='O'and br["text"]=='O'or
@@ -46,11 +87,12 @@ def checkWinner():
         ur["text"]=='O' and mr["text"]=='O'and br["text"]=='O'):
         winner=True
         messagebox.showinfo("TicTacToe", "Player O wins!")
+        disableButtons()
+        
     elif count==9 and winner==False:
         messagebox.showinfo("TicTacToe", "Draw")
-
-
-
+        disableButtons()
+        return 0 
 
 ul = Button(myWindow, text="", width=6, height=3, font=('Helvica',24), command=lambda:btnClick(ul))
 um = Button(myWindow, text="", width=6, height=3, font=('Helvica',24), command=lambda:btnClick(um))
@@ -62,6 +104,16 @@ bl = Button(myWindow, text="", width=6, height=3, font=('Helvica',24), command=l
 bm = Button(myWindow, text="", width=6, height=3, font=('Helvica',24), command=lambda:btnClick(bm))
 br = Button(myWindow, text="", width=6, height=3, font=('Helvica',24), command=lambda:btnClick(br))
 
+major=Menu(myWindow)
+myWindow.config(menu=major)
+
+options=Menu(major, tearoff=False)
+major.add_cascade(label="Options", menu=options)
+options.add_command(label="Play again", command=reset)
+options.add_command(label="Close", command=myWindow.quit)
+
+
+
 ul.grid(column = 0, row = 0)
 um.grid(column = 0, row = 1)
 ur.grid(column = 0, row = 2)
@@ -71,15 +123,5 @@ mr.grid(column = 1, row = 2)
 bl.grid(column = 2, row = 0)
 bm.grid(column = 2, row = 1)
 br.grid(column = 2, row = 2)
-
-
-
-
-
-
-
-
-
-
 
 myWindow.mainloop()
